8b50884c890531fbea93a82c17ffa740e3c95650
FakhriAm <fakhri_muhammad93@yahoo.com>
2020-09-24 15:49:13 +0700

    add style navbar

--- application/views/part/js.php
+++ application/views/part/js.php
@@ -3,7 +3,7 @@
       </div>
       <!-- End of Main Content -->
       <!-- Footer -->
-      <footer class="sticky-footer bg-white">
+      <footer class="sticky-footer">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
             <span>Copyright &copy; Video Content Provider - CNN Indonesia</span>
--- application/views/part/sidebar.php
+++ application/views/part/sidebar.php
@@ -4,8 +4,8 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
   <!-- Sidebar - Brand -->
   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
-    <!-- <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
+    <!-- <div class="sidebar-brand-icon">
+     <img class="img-profile rounded-circle" src="<?php echo base_url('asset/template/img/trans.png')?>" width="50" height="50">
     </div> -->
     <div class="sidebar-brand-text mx-3">Video Content Provider</div>
   </a>
