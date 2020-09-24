be0393e7d74d305be2127a1a9af96ce48d23df44
FakhriAm <fakhri_muhammad93@yahoo.com>
2020-09-24 10:38:02 +0700

    Complete view - Add User

--- application/views/add_user/add_user_view.php
+++ application/views/add_user/add_user_view.php
@@ -10,27 +10,9 @@
 <div class="collapse show" id="video_metadata">
   <div class="card-body">
     <form id="formuploader" enctype="multipart/form-data">
-      <!-- <div class="form-group">
-          <div class="row">
-            <p class="col-2 text-right">Choose Video</p>
-            <div class="col-md-9 mb-3">
-              <input type="file" class="form-control custom-file-input" id="videofile" name="videofile">
-              <label class="custom-file-label" id="label-video" for="customFile">Choose file</label>
-            </div>
-          </div>
-        </div>
         <div class="form-group">
           <div class="row">
-            <p class="col-2 text-right">Choose Video Thumbnail</p>
-            <div class="col-md-9 mb-3">
-              <input type="file" class="form-control custom-file-input" id="imagefile" name="thumbfile">
-              <label class="custom-file-label" id="label-thumb" for="customFile">Choose file</label>
-            </div>
-          </div>
-        </div> -->
-        <div class="form-group">
-          <div class="row">
-            <label class="label col-2 text-left">Employee NIK</label>
+            <label class="label col-2 text-right">Employee NIK</label>
             <div class="col-md-9">
               <input type="text" class="form-control" placeholder="Employee NIK" name="emp_nik">
             </div>
@@ -38,7 +20,7 @@
         </div>
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Employee Name</label>
+            <label class="label col-2 text-right">Employee Name</label>
             <div class="col-md-9">
               <input type="text" class="form-control" placeholder="Employee Name" name="emp_name">
             </div>
@@ -46,7 +28,7 @@
         </div>
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Company</label>
+            <label class="label col-2 text-right">Company</label>
             <div class="col-md-9">
               <input type="text" class="form-control" placeholder="Employee Companion" name="emp_comp">
             </div>
@@ -54,7 +36,7 @@
         </div>
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Division</label>
+            <label class="label col-2 text-right">Division</label>
             <div class="col-md-9">
               <?php echo $filter_category;?>
             </div>
@@ -62,7 +44,7 @@
         </div>
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Department</label>
+            <label class="label col-2 text-right">Department</label>
             <div class="col-md-9">
               <?php echo $filter_category;?>
             </div>
@@ -70,7 +52,7 @@
         </div>
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Employee Phone</label>
+            <label class="label col-2 text-right">Employee Phone</label>
             <div class="col-md-9">
               <input type="text" class="form-control" placeholder="Employee Phone Number < CUG >" name="emp_phone">
             </div>
@@ -78,50 +60,20 @@
         </div>
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Employee Email</label>
+            <label class="label col-2 text-right">Employee Email</label>
             <div class="col-md-9">
               <input type="text" class="form-control" placeholder="Employee Email" name="emp_email">
             </div>
           </div>
         </div>
-        <!-- <div class="form-group">
-          <div class="row">
-            <label class="label col-2 text-left">Description</label>
-            <div class="col-md-9">
-              <textarea class="form-control" placeholder="Enter Video Description" name="desc"></textarea>
-            </div>
-          </div>
-        </div> -->
-        <!-- <div class="form-group">
-          <div class="row">
-            <label class="label col-2 text-left">Duration</label>
-            <div class="col-md-3">
-              <input class="form-control" id='hour' name='hour' type='number' min='0' max='99' placeholder="Hour">
-            </div>
-            <div class="col-md-3">
-              <input class="form-control" id='minute' name='minute' type='number' min='0' max='59' placeholder="Minute">
-            </div>
-            <div class="col-md-3">
-              <input class="form-control" id='second' name='second' name='second' type='number' min='0' max='59' placeholder="Second">
-            </div>
-          </div>
-        </div> -->
         <div class="form-group">
           <div class="row">
-            <label class="label col-2 text-left">Acces Level</label>
+            <label class="label col-2 text-right">Acces Level</label>
             <div class="col-md-9">
               <?php echo $filter_category;?>
             </div>
           </div>
         </div>
-        <!-- <div class="form-group">
-          <div class="row">
-            <label class="label col-2 text-left">Video tag</label>
-            <div class="col-md-9">
-              <textarea class="form-control" placeholder="ex: Pemerintah,Politik,Nama Tokoh,Jakarta" name="tag"></textarea>
-            </div>
-          </div>
-        </div> -->
     </form>
     <div class="button-footer">
         <button  type="button" class="btn btn-info btn-right" onclick="save()">
